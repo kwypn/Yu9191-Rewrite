91115a27962d6d9da2c39152c1f0e4f4cdea4e3a
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-09-17 14:07:24 +0800

    更新 FTzhongwenwang.js

--- FTzhongwenwang.js
+++ FTzhongwenwang.js
@@ -3,11 +3,12 @@ FT中文网 外区
 ***********************
 
 [rewrite_local]
-^http[s]?:\/\/.*.cloudfront.net\/index.php\/jsapi\/paywall url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/FTzhongwenwang.js
+^https:\/\/dqbam2jv6gg9m\.cloudfront\.net\/index\.php\/jsapi\/paywall url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/FTzhongwenwang.js
 [mitm] 
 hostname = .*.cloudfront.net
 
 *******************************/
+var objc = JSON.parse($response.body);
+    objc = {"paywall":0,"premium":1,"standard":1,"addon":0,"expire":"999693756810","v":1997,"campaign_code":"","latest_duration":"yearly"}
+$done({body : JSON.stringify(objc)});
 
-var newBody = '{"paywall":0,"premium":1,"standard":1,"addon":0,"expire":"999693756810","v":1997,"campaign_code":"","latest_duration":"yearly"}';
-$done({newBody});
