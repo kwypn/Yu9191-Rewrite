5ad8978f05bc919cd12d4b5354844a4adac7fe0b
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-09-17 13:11:32 +0800

    Create FTzhongwenwang.js

---
+++ FTzhongwenwang.js
@@ -0,0 +1,16 @@
+/******************************
+FT中文网 外区
+***********************
+
+[rewrite_local]
+^https:\/\/*\.cloudfront\.net\/index\.php\/jsapi\/paywall url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/FTzhongwenwang.js
+
+^^https:\/\/*\.cloudfront\.net\/index\.php\/jsapi\/get_story_more_info\/* url script- response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/FTbody.js
+
+
+[mitm]
+hostname = *.cloudfront.net
+
+*******************************/
+var newBody = '{"paywall":0,"premium":1,"standard":1,"addon":0,"expire":"999693756810","v":1997,"campaign_code":"","latest_duration":"yearly"}';
+$done(newBody);
