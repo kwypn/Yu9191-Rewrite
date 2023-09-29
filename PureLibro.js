ad940b65035048da268ce7aeb793d6b36286d254
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-09-29 19:06:19 +0800

    Update PureLibro.js

--- PureLibro.js
+++ PureLibro.js
@@ -17,4 +17,54 @@ https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body 
 hostname = api.revenuecat.com
 
 *******************************/
-var version_='jsjiami.com.v7';var _0x5cfe7e=_0x58f6;(function(_0x30340a,_0x31e8cb,_0x303037,_0x269406,_0x4e3ab2,_0x33b729,_0x2b656c){return _0x30340a=_0x30340a>>0x2,_0x33b729='hs',_0x2b656c='hs',function(_0x3bd707,_0x509858,_0x12980a,_0x96d413,_0x6a8401){var _0x589066=_0x58f6;_0x96d413='tfi',_0x33b729=_0x96d413+_0x33b729,_0x6a8401='up',_0x2b656c+=_0x6a8401,_0x33b729=_0x12980a(_0x33b729),_0x2b656c=_0x12980a(_0x2b656c),_0x12980a=0x0;var _0x217b2a=_0x3bd707();while(!![]&&--_0x269406+_0x509858){try{_0x96d413=parseInt(_0x589066(0x1b2,'YHt4'))/0x1+-parseInt(_0x589066(0x1af,'Y9CB'))/0x2*(-parseInt(_0x589066(0x1a5,'H]V]'))/0x3)+parseInt(_0x589066(0x1ab,'fS]z'))/0x4+parseInt(_0x589066(0x19c,'y4SA'))/0x5+parseInt(_0x589066(0x199,'F$1H'))/0x6*(parseInt(_0x589066(0x1b1,'DPG9'))/0x7)+-parseInt(_0x589066(0x1a6,'p$9s'))/0x8*(-parseInt(_0x589066(0x198,'H]V]'))/0x9)+-parseInt(_0x589066(0x19e,'&pd('))/0xa;}catch(_0x39c14c){_0x96d413=_0x12980a;}finally{_0x6a8401=_0x217b2a[_0x33b729]();if(_0x30340a<=_0x269406)_0x12980a?_0x4e3ab2?_0x96d413=_0x6a8401:_0x4e3ab2=_0x6a8401:_0x12980a=_0x6a8401;else{if(_0x12980a==_0x4e3ab2['replace'](/[qtCwUngQJrNSEXRAuFOh=]/g,'')){if(_0x96d413===_0x509858){_0x217b2a['un'+_0x33b729](_0x6a8401);break;}_0x217b2a[_0x2b656c](_0x6a8401);}}}}}(_0x303037,_0x31e8cb,function(_0x233188,_0x5caab1,_0x1f5355,_0x2a2a92,_0xecbcbc,_0x20216a,_0x4d7904){return _0x5caab1='\x73\x70\x6c\x69\x74',_0x233188=arguments[0x0],_0x233188=_0x233188[_0x5caab1](''),_0x1f5355='\x72\x65\x76\x65\x72\x73\x65',_0x233188=_0x233188[_0x1f5355]('\x76'),_0x2a2a92='\x6a\x6f\x69\x6e',(0x140427,_0x233188[_0x2a2a92](''));});}(0x324,0x56857,_0x2ff7,0xcb),_0x2ff7)&&(version_=_0x2ff7);function _0x2ff7(){var _0x547c41=(function(){return[version_,'StjsujqiQXarmnEih.JgcAom.Rv7nOQwNFUOCSrq==','W7bUzdpdJu/dGSkf','WP9+WRGtvCkDW612WPhdM1DZ','W5vBnCoUbCo5','D8oUECkRW5VcLSovWQhcMW','WOKeDCkWsCkLdbldKSksW4FcOutdL8kIW57dGY7cTmoA','ASkbcCkNW4VdIau9y8otWOvj','CCo9pcX4o8khtCksW53dGmkPWPitW6hcGmovu3CdpCk6WP0PWORdH8k1zCojuxNcU8ozWOJcUWa0W7pcSmkUWRjltSkd','ACoOoI9U','WODDWQ8vWO0rzSk7bmk/ECk3W61XW6WwWQTKW70y','W51UC8k7W77dM8kl','W4VcKCoeiCobASo0W5FdQumSfmo/'].concat((function(){return['aJmermo7W5jPWP8','WOq/W5/dV8oNW4FcImouDSoQWPuU','WOu7WRJcJmkKWQZcQ8ow','WQ0fD8kC','k3RdOv9tW4SFW4nS','WODDWQ8vWO0rzSk7bmk/ECk3W61XW6WrWQTHW7Sy','WODxWQdcSCkhWQhcJW','hSocW7/cV8kvweytchm3w8ojv00VW5HhDaK','vmkFv8ktpKixWQddP8onW7TWWO7dPG','xtFcQaqLsJWmWQX+WQpcSa','yfldUCk3vaDQsdelWPpdJKq','xJxdQLL9frup','WRP+E8oWkCkwFmkJsSoklmkmCYpcJSkAE8oBWPTC'].concat((function(){return['WR9+FmoWnSkweSoNemkEmmo8','BsVdVrLLWRKpu37dI2pdMG','WOOcuGyQFcqRW7TbCmoFpaa','W5bGWOxdP8kKWPdcO8ofE8o9W7iGzmoyomo8WRbcomo8h8oxWRXZW4i1W5CvWQRcIq','AYpdVq','y3JdRMZdPSkskadcGLJdLmkAWPDhW7qadcldTMG','DCktW5pcSSkjW6P8WODNiSoBlevUW6G','WQNdTmkUvSk/hSkFW5ddQa','x8kGm2DLWP4Sid7dRmkpBGzBFHKXt8o7yG','wtNcOaWHsWCzWRL8WOZcRW'];}()));}()));}());_0x2ff7=function(){return _0x547c41;};return _0x2ff7();}function _0x58f6(_0x16c009,_0x18da87){var _0x2ff74c=_0x2ff7();return _0x58f6=function(_0x58f616,_0x1eba45){_0x58f616=_0x58f616-0x197;var _0x167009=_0x2ff74c[_0x58f616];if(_0x58f6['sEjHaE']===undefined){var _0x23f346=function(_0x5f49e8){var _0x4e95e0='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';var _0x37a5dc='',_0x392d7d='';for(var _0x5e6ec1=0x0,_0x3f3dfb,_0x195ec7,_0x4637aa=0x0;_0x195ec7=_0x5f49e8['charAt'](_0x4637aa++);~_0x195ec7&&(_0x3f3dfb=_0x5e6ec1%0x4?_0x3f3dfb*0x40+_0x195ec7:_0x195ec7,_0x5e6ec1++%0x4)?_0x37a5dc+=String['fromCharCode'](0xff&_0x3f3dfb>>(-0x2*_0x5e6ec1&0x6)):0x0){_0x195ec7=_0x4e95e0['indexOf'](_0x195ec7);}for(var _0x2e7e4f=0x0,_0x7b4b98=_0x37a5dc['length'];_0x2e7e4f<_0x7b4b98;_0x2e7e4f++){_0x392d7d+='%'+('00'+_0x37a5dc['charCodeAt'](_0x2e7e4f)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(_0x392d7d);};var _0x1af68f=function(_0x59da34,_0x362673){var _0x48e77f=[],_0x3dd2dd=0x0,_0x22a007,_0xcbda1c='';_0x59da34=_0x23f346(_0x59da34);var _0x414e7f;for(_0x414e7f=0x0;_0x414e7f<0x100;_0x414e7f++){_0x48e77f[_0x414e7f]=_0x414e7f;}for(_0x414e7f=0x0;_0x414e7f<0x100;_0x414e7f++){_0x3dd2dd=(_0x3dd2dd+_0x48e77f[_0x414e7f]+_0x362673['charCodeAt'](_0x414e7f%_0x362673['length']))%0x100,_0x22a007=_0x48e77f[_0x414e7f],_0x48e77f[_0x414e7f]=_0x48e77f[_0x3dd2dd],_0x48e77f[_0x3dd2dd]=_0x22a007;}_0x414e7f=0x0,_0x3dd2dd=0x0;for(var _0x2561fc=0x0;_0x2561fc<_0x59da34['length'];_0x2561fc++){_0x414e7f=(_0x414e7f+0x1)%0x100,_0x3dd2dd=(_0x3dd2dd+_0x48e77f[_0x414e7f])%0x100,_0x22a007=_0x48e77f[_0x414e7f],_0x48e77f[_0x414e7f]=_0x48e77f[_0x3dd2dd],_0x48e77f[_0x3dd2dd]=_0x22a007,_0xcbda1c+=String['fromCharCode'](_0x59da34['charCodeAt'](_0x2561fc)^_0x48e77f[(_0x48e77f[_0x414e7f]+_0x48e77f[_0x3dd2dd])%0x100]);}return _0xcbda1c;};_0x58f6['EIPoJr']=_0x1af68f,_0x16c009=arguments,_0x58f6['sEjHaE']=!![];}var _0x133e06=_0x2ff74c[0x0],_0x452fcb=_0x58f616+_0x133e06,_0x2ee7e9=_0x16c009[_0x452fcb];return!_0x2ee7e9?(_0x58f6['GTDsVB']===undefined&&(_0x58f6['GTDsVB']=!![]),_0x167009=_0x58f6['EIPoJr'](_0x167009,_0x1eba45),_0x16c009[_0x452fcb]=_0x167009):_0x167009=_0x2ee7e9,_0x167009;},_0x58f6(_0x16c009,_0x18da87);};var objc=JSON[_0x5cfe7e(0x1ad,'NvV9')]($response[_0x5cfe7e(0x1b4,'rA5R')]);objc={'request_date_ms':0x18ad6f43b30,'request_date':_0x5cfe7e(0x1ae,'RD1F'),'subscriber':{'non_subscriptions':{},'first_seen':'2023-09-27T13:53:14Z','original_application_version':_0x5cfe7e(0x1a0,'#ByF'),'other_purchases':{},'management_url':_0x5cfe7e(0x1ac,'NvV9'),'subscriptions':{'com.reader.autoRenewableYearly':{'original_purchase_date':_0x5cfe7e(0x1a4,'[Xdn'),'expires_date':_0x5cfe7e(0x1b8,'8Xnc'),'is_sandbox':![],'refunded_at':null,'store_transaction_id':_0x5cfe7e(0x1a2,'A14G'),'unsubscribe_detected_at':null,'grace_period_expires_date':null,'period_type':_0x5cfe7e(0x1a8,'bDy]'),'purchase_date':_0x5cfe7e(0x1b6,'RD1F'),'billing_issues_detected_at':null,'ownership_type':_0x5cfe7e(0x1a3,'i7kK'),'store':_0x5cfe7e(0x1a9,'Gq3H'),'auto_resume_date':null}},'entitlements':{'subscription.pro':{'grace_period_expires_date':null,'purchase_date':_0x5cfe7e(0x1aa,'bDy]'),'product_identifier':_0x5cfe7e(0x19f,'YHt4'),'expires_date':_0x5cfe7e(0x1a1,'nKzw')}},'original_purchase_date':_0x5cfe7e(0x19b,'y4SA'),'original_app_user_id':'$RCAnonymousID:e2b381c2f7204bf69899c1bc1318bb90','last_seen':'2023-09-27T13:53:14Z'}},$done({'body':JSON[_0x5cfe7e(0x1b5,'4e!)')](objc)});var version_ = 'jsjiami.com.v7';
+var objc = JSON.parse($response.body);
+
+    objc = 
+
+{
+  "request_date_ms" : 1695823444784,
+  "request_date" : "2023-09-27T14:04:04Z",
+  "subscriber" : {
+    "last_seen" : "2023-09-27T13:53:14Z",
+    "first_seen" : "2023-09-27T13:53:14Z",
+    "original_application_version" : "187",
+    "other_purchases" : {
+
+    },
+    "management_url" : "https://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "reader.lifetime.pro" : {
+        "store_transaction_id" : "190001736542492",
+        "expires_date" : "2099-10-27T14:03:52Z",
+        "is_sandbox" : false,
+        "refunded_at" : null,
+        "unsubscribe_detected_at" : null,
+        "auto_resume_date" : null,
+        "grace_period_expires_date" : null,
+        "period_type" : "normal",
+        "purchase_date" : "2023-09-27T14:03:52Z",
+        "billing_issues_detected_at" : null,
+        "ownership_type" : "PURCHASED",
+        "store" : "app_store",
+        "original_purchase_date" : "2023-09-27T14:03:55Z"
+      }
+    },
+    "entitlements" : {
+      "pro" : {
+        "expires_date" : "2099-10-27T14:03:52Z",
+        "purchase_date" : "2023-09-27T14:03:52Z",
+        "product_identifier" : "reader.lifetime.pro",
+        "grace_period_expires_date" : null
+      }
+    },
+    "original_purchase_date" : "2023-09-27T13:52:21Z",
+    "original_app_user_id" : "$RCAnonymousID:e2b381c2f7204bf69899c1bc1318bb90",
+    "non_subscriptions" : {
+
+    }
+  }
+}
+
+
+
+$done({body : JSON.stringify(objc)});
